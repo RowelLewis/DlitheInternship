6501f1a254d18827d13d0dda639e3b27603e9204
RowelLewis <rowellewis550@gmail.com>
2020-09-10 22:27:02 +0530

    second commit - includes a new bike.java class file with private data members and public methods to access these private data members

---
+++ project1/src/main/java/internship/rowel/project1/Bike.java
@@ -0,0 +1,54 @@
+package internship.rowel.project1;
+
+public class Bike {
+	private String model=""; private Integer year=0, milage=0, cc=0; private Double cost=0.0;
+	
+	public Bike(String model, Integer year, Integer milage, Integer cc, Double cost) {
+		this.model = model;
+		this.year = year;
+		this.milage = milage;
+		this.cc = cc;
+		this.cost = cost;
+	}
+	@Override
+	public String toString() {
+		return "Bike [model=" + model + ", year=" + year + ", milage=" + milage + ", cc=" + cc + ", cost=" + cost + "]";
+	}
+	public Integer getYear() {
+		return year;
+	}
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+	public Integer getMilage() {
+		return milage;
+	}
+	public void setMilage(Integer milage) {
+		this.milage = milage;
+	}
+	public Integer getCc() {
+		return cc;
+	}
+	public void setCc(Integer cc) {
+		this.cc = cc;
+	}
+	public Double getCost() {
+		return cost;
+	}
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+	
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getModel() {
+		return model;
+	}
+	
+	public void displayAll() {
+		System.out.println(this.model+" "+this.year+" "+this.milage+" "+this.cc+" "+this.cost);
+	}
+	
+}
